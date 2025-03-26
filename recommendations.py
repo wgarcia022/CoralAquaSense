5660fb8a47ecf14d7ad0c0477a018793e5ebd226
wgarcia022 <waldo.garcia@sjsu.edu>
2025-03-26 13:02:15 -0700

    Create recommendations.py

---
+++ recommendations.py
@@ -0,0 +1,3 @@
+#Implements the logic for generating personalized irrigation advice based on the data from data_fetch.py.
+#Uses AI algorithms or rule-based logic to analyze current conditions and user-specific inputs 
+#(such as crop type or water usage patterns) to generate actionable insights.
