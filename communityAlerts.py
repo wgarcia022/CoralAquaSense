5140909437cfb5dbb4d6b03ddc04623ac8b09d7f
wgarcia022 <waldo.garcia@sjsu.edu>
2025-03-26 13:05:31 -0700

    Create communityAlerts.py

---
+++ communityAlerts.py
@@ -0,0 +1,3 @@
+#Manages the functionality for sending and displaying community alerts.
+#Integrates Streamlit’s messaging components to notify users about real-time 
+  #updates, such as collective water consumption trends and conservation initiatives.
